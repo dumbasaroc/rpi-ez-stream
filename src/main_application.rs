6d90970d171578b130774fde8c569fb988df3998
DumbAsARoc <timbishop514@gmail.com>
2025-01-30 14:58:59 -0500

    refactored mainwindow and added a test global shortcut

--- src/main_application.rs
+++ src/main_application.rs
@@ -1,9 +1,10 @@
 //! Contains the struct that initializes and runs
 //! the application.
 
-use log::info;
+use log::debug;
 use gtk4::prelude::*;
 use gtk4::Application;
+use gtk4::gio::ActionEntry;
 
 use super::ui;
 
@@ -27,21 +28,36 @@ impl MainApplication {
     /// A new MainApplication struct, ready to
     /// start running.
     pub fn new() -> Self {
+        debug!("Creating application...");
         let m_app = MainApplication {
             app: Application::builder()
                 .application_id(APPLICATION_ID)
                 .build()
         };
 
+        debug!("Connecting activation callback to application...");
         m_app.app.connect_activate(|app| {
+            debug!("Creating main window, attaching to application...");
             let mainwindow = ui::MainWindow::new(app);
 
+            debug!("Setting P1 Button click callback...");
             mainwindow.shown_screen().p1_character().connect_clicked(|_b| {
                 println!("Clicked p1 character button!");
             });
 
+            debug!("Creating a dummy action to close the window!");
+            let action_close = ActionEntry::builder("close")
+                .activate(|window: &ui::MainWindow, _, _| {
+                    window.close();
+                })
+                .build();
+            mainwindow.add_action_entries([action_close]);
+
             mainwindow.present();
         });
+
+        debug!("Adding shortcut for closing the application.");
+        m_app.app.set_accels_for_action("win.close", &["<Ctrl>W"]);
 
         m_app
     }
@@ -53,6 +69,7 @@ impl MainApplication {
     /// An ExitCode that will be reported to the runing
     /// terminal.
     pub fn run_application(&self) -> gtk4::glib::ExitCode {
+        debug!("Running application!");
         self.app.run()
     }
 }
--- src/ui/main_window.rs
+++ src/ui/main_window.rs
@@ -1,6 +1,3 @@
-use gtk4::glib::Properties;
-use gtk4::subclass::prelude::*;
-use gtk4::prelude::*;
 use gtk4::glib;
 use gtk4::*;
 
@@ -8,7 +5,7 @@ use super::MainScreen;
 
 glib::wrapper! {
 
-    pub struct MainWindow(ObjectSubclass<priv_MainWindow>)
+    pub struct MainWindow(ObjectSubclass<imp::MainWindow>)
         @extends gtk4::ApplicationWindow, gtk4::Window, gtk4::Widget,
         @implements gio::ActionGroup, gio::ActionMap, gtk4::Buildable;
 
@@ -21,42 +18,52 @@ impl MainWindow {
     }
 }
 
+mod imp {
 
-#[allow(non_camel_case_types)]
-#[derive(Default, CompositeTemplate, Properties)]
-#[properties(wrapper_type = MainWindow)]
-#[template(file = "main_window.ui")]
-pub struct priv_MainWindow {
+    use gtk4::glib::Properties;
+    use gtk4::subclass::prelude::*;
+    use gtk4::prelude::*;
+    use gtk4::glib;
+    use gtk4::*;
 
-    #[template_child]
-    #[property(get)]
-    pub shown_screen: TemplateChild<MainScreen>
-}
+    use super::MainScreen;
 
-#[glib::object_subclass]
-impl ObjectSubclass for priv_MainWindow {
+    #[allow(non_camel_case_types)]
+    #[derive(Default, CompositeTemplate, Properties)]
+    #[properties(wrapper_type = super::MainWindow)]
+    #[template(file = "main_window.ui")]
+    pub struct MainWindow {
 
-    const NAME: &'static str = "MainWindow";
-    type Type = MainWindow;
-    type ParentType = gtk4::ApplicationWindow;
-
-
-    fn class_init(klass: &mut Self::Class) {
-        klass.bind_template();
+        #[template_child]
+        #[property(get)]
+        pub shown_screen: TemplateChild<MainScreen>
     }
 
-    fn instance_init(obj: &glib::subclass::InitializingObject<Self>) {
-        obj.init_template();
+    #[glib::object_subclass]
+    impl ObjectSubclass for MainWindow {
+
+        const NAME: &'static str = "MainWindow";
+        type Type = super::MainWindow;
+        type ParentType = gtk4::ApplicationWindow;
+
+
+        fn class_init(klass: &mut Self::Class) {
+            klass.bind_template();
+        }
+
+        fn instance_init(obj: &glib::subclass::InitializingObject<Self>) {
+            obj.init_template();
+        }
     }
+
+
+    impl MainWindow {}
+
+
+    impl ApplicationWindowImpl for MainWindow {}
+    impl WindowImpl for MainWindow {}
+    impl WidgetImpl for MainWindow {}
+
+    #[glib::derived_properties]
+    impl ObjectImpl for MainWindow {}
 }
-
-
-impl priv_MainWindow {}
-
-
-impl ApplicationWindowImpl for priv_MainWindow {}
-impl WindowImpl for priv_MainWindow {}
-impl WidgetImpl for priv_MainWindow {}
-
-#[glib::derived_properties]
-impl ObjectImpl for priv_MainWindow {}
