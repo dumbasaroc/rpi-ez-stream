4c4ead2236bfad1760994b5178192374e2882298
DumbAsARoc <timbishop514@gmail.com>
2025-02-07 17:15:11 -0500

    reformatted character data to be more verbose in anticipation of character button functionality

--- src/application_data/player_data.rs
+++ src/application_data/player_data.rs
@@ -1,3 +1,5 @@
+use log::warn;
+
 #[derive(serde::Serialize)]
 pub struct PlayerData {
 
@@ -10,7 +12,18 @@ pub struct PlayerData {
     /// Player character data - can be
     /// none (especially if characters
     /// can't be selected.)
-    player_character: Option<String>,
+    player_character: Option<CharacterData>,
+}
+
+#[derive(Clone, serde::Serialize)]
+struct CharacterData {
+    
+    /// The name of the character, should match
+    /// some internal hashmap @TODO TBD
+    pub character_name: String,
+
+    /// The costume index currently selected.
+    pub costume_number: u32,
 }
 
 
@@ -26,8 +39,8 @@ impl PlayerData {
         &self.player_name
     }
 
-    pub fn character(&self) -> Option<&str> {
-        self.player_character.as_deref()
+    pub fn character(&self) -> Option<&CharacterData> {
+        self.player_character.as_ref()
     }
 
     
@@ -41,11 +54,40 @@ impl PlayerData {
         self.player_name = name.into();
     }
 
-    pub fn set_character(&mut self, character: Option<impl Into<String>>) {
-        self.player_character = match character {
-            Some(s) => Some(s.into()),
+    pub fn set_character_costume(&mut self, costume: u32) {
+        match &mut self.player_character {
+            Some(c) => {
+                c.costume_number = costume;
+            },
+            None => {
+                warn!("Attempting to set {}'s NULL character's costume to {}!",
+                    self.name(),
+                    costume
+                );
+            }
+        }
+    }
+
+    /// Sets the character to a specific character, or no character
+    /// depending on the optional parameter.
+    /// 
+    /// # Note
+    /// This method resets the costume to idx zero if given the
+    /// name parameter.
+    pub fn set_character<S>(&mut self, name: Option<S>)
+        where S: AsRef<String>
+    {
+        let c = match name {
+            Some(n) => {
+                Some(CharacterData {
+                    character_name: format!("{}", n.as_ref()),
+                    costume_number: 0
+                })
+            },
             None => None
         };
+
+        self.player_character = c;
     }
 
 }
